9452afdfe4f8b7c07a9c87cb4f7f0bcfa1bd6960
Trilamdoan <106875432+Trilamdoan@users.noreply.github.com>
2022-06-08 16:31:31 +0700

    test

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	private ten;
+
+	public Class1()
+	{
+		//Test thử push code
+		// TODO: Add constructor logic here
+		//
+		
+	}
+}
